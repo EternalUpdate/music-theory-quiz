cf682fa7ec7bf5e32eee37a8197e00ab0ed3e2e7
Eugenie Tremblay <eugenie.tremblay@gmail.com>
2023-06-19 19:50:16 -0400

    Added settings and basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import "./App.css";
-import { Heading, Text, Input, Button, VStack } from "@chakra-ui/react";
+import { Heading, Text, Input, Button, VStack, HStack } from "@chakra-ui/react";
 import {
     ChordGradesQuestion,
     getChordGradesQuestion,
 } from "./questions/ChordGradesQuestions";
+import KeyChoices from "./components/KeyChoices";
+import ModeChoices from "./components/ModeChoices";
 
 function App() {
     const [question, setQuestion] = useState<ChordGradesQuestion | null>(null);
@@ -34,23 +35,77 @@ function App() {
         }
     };
 
+    const handleKeyChoiceChange = () => {
+        console.log("key choice changed");
+    };
+
     return (
         <>
-            <Heading>Music Theory Quiz</Heading>
-            <VStack spacing={8} mt={10}>
-                {started ? <Text fontSize="lg">Score: {score}</Text> : ""}
-                <Text fontSize="2xl">{question?.question}</Text>
-                {started ? (
-                    <Input
-                        value={answer}
-                        size="lg"
-                        variant="flushed"
-                        onChange={handleInputChange}
-                    />
-                ) : (
-                    ""
-                )}
-                {started ? "" : <Button onClick={handleBegin}>Begin</Button>}
+            <VStack p="2rem">
+                <Text fontSize="md" fontWeight={500} color={"gray.800"}>
+                    Music Theory Quiz
+                </Text>
+                <VStack
+                    spacing={8}
+                    mt={10}
+                    p={20}
+                    border={"8px"}
+                    borderColor={"teal.300"}
+                    borderRadius={"100px"}
+                    maxWidth={"70%"}
+                >
+                    {started ? (
+                        <Text fontSize="lg" color={"gray.500"}>
+                            Score: {score}
+                        </Text>
+                    ) : (
+                        ""
+                    )}
+                    <Text fontSize="2xl" fontWeight={500} color={"gray.800"}>
+                        {question?.question}
+                    </Text>
+                    {started ? (
+                        <Input
+                            value={answer}
+                            size="lg"
+                            maxWidth={"5rem"}
+                            textAlign={"center"}
+                            border={"2px"}
+                            borderColor={"gray.300"}
+                            focusBorderColor={"gray.300"}
+                            autoFocus
+                            onChange={handleInputChange}
+                        />
+                    ) : (
+                        ""
+                    )}
+                    {started ? (
+                        ""
+                    ) : (
+                        <Button onClick={handleBegin}>Begin</Button>
+                    )}
+                </VStack>
+                <VStack mt={20}>
+                    <Heading size="lg" color={"gray.800"}>
+                        Settings
+                    </Heading>
+                    <VStack>
+                        <Heading size="md" color={"gray.800"}>
+                            Key
+                        </Heading>
+                        <HStack maxWidth={"50%"} wrap={"wrap"} spacing={5}>
+                            <KeyChoices onChange={handleKeyChoiceChange} />
+                        </HStack>
+                    </VStack>
+                    <VStack>
+                        <Heading size="md" color={"gray.800"}>
+                            Mode
+                        </Heading>
+                        <HStack maxWidth={"50%"} wrap={"wrap"} spacing={5}>
+                            <ModeChoices onChange={handleKeyChoiceChange} />
+                        </HStack>
+                    </VStack>
+                </VStack>
             </VStack>
         </>
     );
