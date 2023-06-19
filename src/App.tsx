395cb597ee3b5a8645c5411fb9f5214c634042c1
Eugenie Tremblay <eugenie.tremblay@gmail.com>
2023-06-19 17:29:36 -0400

    Added basic quiz

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,57 @@
 import { useState } from "react";
 import "./App.css";
-import { Heading } from "@chakra-ui/react";
+import { Heading, Text, Input, Button, VStack } from "@chakra-ui/react";
+import {
+    ChordGradesQuestion,
+    getChordGradesQuestion,
+} from "./questions/ChordGradesQuestions";
 
 function App() {
+    const [question, setQuestion] = useState<ChordGradesQuestion | null>(null);
+    const [answer, setAnswer] = useState("");
+    const [started, setStarted] = useState(false);
+    const [score, setScore] = useState(0);
+
+    const handleBegin = () => {
+        setStarted(true);
+        getNextQuestion();
+    };
+
+    const getNextQuestion = () => {
+        setQuestion(getChordGradesQuestion());
+    };
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setAnswer(event.target.value);
+        validateAnswer();
+    };
+
+    const validateAnswer = () => {
+        if (question?.answer.toLowerCase() === answer.toLocaleLowerCase()) {
+            setAnswer("");
+            setScore(score + 1);
+            getNextQuestion();
+        }
+    };
+
     return (
         <>
             <Heading>Music Theory Quiz</Heading>
+            <VStack spacing={8} mt={10}>
+                {started ? <Text fontSize="lg">Score: {score}</Text> : ""}
+                <Text fontSize="2xl">{question?.question}</Text>
+                {started ? (
+                    <Input
+                        value={answer}
+                        size="lg"
+                        variant="flushed"
+                        onChange={handleInputChange}
+                    />
+                ) : (
+                    ""
+                )}
+                {started ? "" : <Button onClick={handleBegin}>Begin</Button>}
+            </VStack>
         </>
     );
 }
