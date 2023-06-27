84d7c4a8071ef673327874e3936be79b8a6961a1
Eugenie Tremblay <eugenie.tremblay@gmail.com>
2023-06-26 23:45:53 -0400

    Routing implementation

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,24 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme.ts";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout.tsx";
+import ChordGradeQuiz from "./pages/ChordGradeQuiz.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <ChakraProvider theme={theme}>
-            <App />
+            <BrowserRouter>
+                <Layout>
+                    <Routes>
+                        <Route path="/" element={<App />} />
+                        <Route
+                            path="chordGradeQuiz"
+                            element={<ChordGradeQuiz />}
+                        />
+                    </Routes>
+                </Layout>
+            </BrowserRouter>
         </ChakraProvider>
     </React.StrictMode>
 );
