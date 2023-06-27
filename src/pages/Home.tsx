f9af032ffd3dad957bed34bfbd1f7286491751b6
Eugenie Tremblay <eugenie.tremblay@gmail.com>
2023-06-26 23:47:57 -0400

    Added home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,26 @@
+import { Heading, VStack, Link } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
+import { Helmet } from "react-helmet";
+
+const Home = () => {
+    return (
+        <>
+            <Helmet>
+                <title>Home - Music Theory Quiz App</title>
+            </Helmet>
+            <VStack marginY={12}>
+                <Heading size="lg" marginY={16} textAlign={"center"}>
+                    What do you want to be tested on?
+                </Heading>
+                <VStack className="all-categories-container">
+                    <Heading size="md">Chord Degrees</Heading>
+                    <Link as={RouterLink} to="/chordGradeQuiz">
+                        What is the IV chord in D dorian?
+                    </Link>
+                </VStack>
+            </VStack>
+        </>
+    );
+};
+
+export default Home;
